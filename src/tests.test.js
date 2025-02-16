72cf2b54c499fcd097a0a2e504f4e9fb99fd966e
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-16 14:08:07 +0400

    tests.test.js

---
+++ src/tests.test.js
@@ -0,0 +1,13 @@
+const { ships } = require("./ships.js");
+
+let ship = new ships();
+let mockShips = jest.fn((shipHit) => {
+	gb.shipHit.hit();
+	return gb.ship.damage;
+});
+
+describe("gameboard", () => {
+	test("hit() increases damage", () => {
+		expect(mockGameboard("carrier")).toBe(1);
+	});
+});
