3d74ec320892ba5fb8aded427c515ea8a99b23ca
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-16 05:44:17 +0400

    gameboard.js

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,16 @@
-export default class gameboard {
+class gameboard {
 	column = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 	row = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 	board = {};
 	setBoard = () => {
-		for (let i = 0; i < 10; i++) {
-			for (let j in this.row) {
-				this.board[`${this.column[i] + this.row[j]}`] = false;
-			}
-		}
+		this.column.forEach((i) => {
+			this.row.forEach((j) => {
+				this.board[`${i + "-" + j}`] = false;
+			});
+		});
 	};
 }
+
+let player1 = new gameboard();
+player1.setBoard();
+console.log(player1);
